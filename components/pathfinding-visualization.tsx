85ccaad624fa611e637fe40bda2981c4e6c8d97c
Chuckz Española <chuckz.espanola99@gmail.com>
2025-12-03 23:13:57 +0800

    Update pathfinding-visualization.tsx

--- components/pathfinding-visualization.tsx
+++ components/pathfinding-visualization.tsx
@@ -538,7 +538,7 @@ export function PathfindingVisualization({
         return;
       } else if (node.type === "custom") {
         // Render custom nodes as a DESSERT TABLE (rectangle, like buffet)
-        const width = 40 * contentScale;
+        const width = 30 * contentScale;
         const height = 120 * contentScale;
       
         // Same dark color as buffet (you can tweak this)
